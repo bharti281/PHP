c990ed658dde0740c48c4833f8df3708787a2fae
Bharti Kumari <bhartikumarikalwari@gmail.com>
2025-05-20 14:27:52 +0530

    new file aaded

--- DataType.php
+++ DataType.php
@@ -8,5 +8,14 @@ echo $n."<br>";
 var_dump($n);
 echo $f."<br>";
 var_dump($f);
+$bool = true."<br>";
+echo $bool;
+var_dump($bool);
+$arry = array("HTML","CSS","JS<br>");
+var_dump($arry);
+$nl = null;
+echo $nl
+var_dump($nl);
+
 
 ?>
